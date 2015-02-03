53ef112ed34c2d7f3b2e43d85bb41d0a4dc788ee
Calen Pennington <cale@edx.org>
2015-02-03 14:38:37 -0500

    fixup! Don't instantiate bogus modulestores just to get template rendering in old ORA

--- common/lib/xmodule/xmodule/peer_grading_module.py
+++ common/lib/xmodule/xmodule/peer_grading_module.py
@@ -497,7 +497,7 @@ class PeerGradingModule(PeerGradingFields, XModule):
         try:
             response = self.peer_gs.save_calibration_essay(**data_dict)
             if 'actual_rubric' in response:
-                rubric_renderer = combined_open_ended_rubric.CombinedOpenEndedRubric(self.render_template, True)
+                rubric_renderer = combined_open_ended_rubric.CombinedOpenEndedRubric(self.system.render_template, True)
                 response['actual_rubric'] = rubric_renderer.render_rubric(response['actual_rubric'])['html']
             return response
         except GradingServiceError:
